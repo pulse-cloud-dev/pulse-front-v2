de94b3fa4d6fdb30c092f78e43d36b69f8a83d89
euan-lee <maplestory1419@naver.com>
2025-05-12 18:49:29 +0900

    feat:라우팅 수정 컨트롤러 수정

--- src/features/auth/finds/find.controller.tsx
+++ src/features/auth/finds/find.controller.tsx
@@ -1,12 +1,10 @@
 import { FindPassWordView } from "./findPassWord.view";
 import { useState } from "react";
 import { usePageNavigation } from "@/shared/lib/hooks";
-import { findService } from "./find.service";
 
 export const FindController = () => {
   // View에서 사용되어질 상태관리
   const [step, setStep] = useState<"인증선택" | "이매일아이디보여주기" | "비밀번호재설정">("인증선택");
-  const { oauthMutation } = findService.useOauth();
 
   //인증선택해서 성공시 이매일아이디보여주기로 이동
   const handleOauthLogin = () => {
@@ -14,20 +12,12 @@ export const FindController = () => {
     setStep("이매일아이디보여주기");
   };
 
-  //비밀번호 재설정 함수
-  const { resetpaswordmutation } = findService.useResetAccountPassword();
+  // //비밀번호 재설정 함수
   const navigation = usePageNavigation();
 
   const handleResetPassword = (payload: any) => {
     navigation.goHome();
-    // resetpaswordmutation.mutate(payload, {
-    //   onSuccess: () => {
-    //     //비밀번호 재설정 성공시 홈으로 이동
-    //     navigation.goHome();
-    //   },
-    // });
   };
-
   // Controller에서 View로 내려질 Props
   const props = {
     state: { step, setStep, handleOauthLogin, handleResetPassword, navigation },
