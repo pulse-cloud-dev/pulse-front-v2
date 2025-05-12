3c167712c62e1b79436dec66cf62c34e6ec90b5f
euan-lee <maplestory1419@naver.com>
2025-05-12 18:07:46 +0900

    feat:라우팅 수정

--- src/features/auth/finds/find.controller.tsx
+++ src/features/auth/finds/find.controller.tsx
@@ -8,10 +8,10 @@ export const FindController = () => {
   const [step, setStep] = useState<"인증선택" | "이매일아이디보여주기" | "비밀번호재설정">("인증선택");
   const { oauthMutation } = findService.useOauth();
 
-  //인증선택해서 성공시
+  //인증선택해서 성공시 이매일아이디보여주기로 이동
   const handleOauthLogin = () => {
     // oauthMutation.mutate(undefined, {});
-    setStep("비밀번호재설정");
+    setStep("이매일아이디보여주기");
   };
 
   //비밀번호 재설정 함수
