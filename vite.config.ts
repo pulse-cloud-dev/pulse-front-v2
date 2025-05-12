b25af16f40b655ecdb1487ae9869643e7209e287
ssddo <ujm0524@naver.com>
2025-05-12 17:40:24 +0900

    [fix][PULSE-228] 절대 경로로 변경

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }: ConfigEnv) => {
 
   // Common settings
   const commonSettings = {
-    base: "./",
+    base: "/",
     plugins: [react(), tsconfigPaths()],
     resolve: {
       alias: {
