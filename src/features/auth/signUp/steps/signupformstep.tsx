e887b33458070ed1e72a9cf12426c95a3ebdb4aa
euan-lee <maplestory1419@naver.com>
2025-05-25 22:16:12 +0900

    bug:nickname변수 수정,form에러검증 로직 버그 수정

--- src/features/auth/signUp/steps/signupformstep.tsx
+++ src/features/auth/signUp/steps/signupformstep.tsx
@@ -90,7 +90,7 @@ const useFormFields = (initialState: FormState) => {
   };
 };
 const initialFields: FormState = {
-  nickname: {
+  nick_name: {
     isValid: false,
     value: "",
     error: "",
@@ -121,25 +121,34 @@ export const SignUpFormStep = ({ onPrev, onNext }: SignUpStepProps) => {
   const { formState, handleInputChange, handleBlur, handleFocus, isFormValid } = useFormFields(initialFields);
   const { checknickname } = useNicknameCheck();
 
+  //debounce를 사용하여 닉네임 중복체크 api 호출
   useEffect(() => {
     //닉네임이 변경될 때마다 검증 로직 실행
-    if (formState.nickname.schema && formState.nickname.schema(formState.nickname.value)) {
+    if (formState.nick_name.schema && formState.nick_name.schema(formState.nick_name.value)) {
       const timer = setTimeout(() => {
         //닉네임이 schema 통과하고 일정시간이 지난다면 닉네임 중복 api 호출
-        checknickname.mutate(formState.nickname.value);
+        checknickname.mutate(formState.nick_name.value);
       }, 500);
 
       return () => clearTimeout(timer);
     }
-  }, [formState.nickname.value]);
+  }, [formState.nick_name.value]);
   // const queryClient = useQueryClient();
   // const { name, phone_number, email } = queryClient.getQueryData(["auth", "signup", "userinfo"]) as SimplifiedUserlResponseDTO;
 
   const { name, phone_number, email } = { name: "김펄스", phone_number: "010-1234-1234", email: "id@pulse.com" };
+
   const onsubmit = (e: FormEvent) => {
-    alert("submitted");
+    e.preventDefault();
+    console.log({
+      name,
+      phone_number,
+      email,
+      nickname: formState.nick_name.value,
+      password: formState.password.value,
+    });
   };
-  console.log("닉 확인", formState.nickname);
+
   return (
     <form className="form__auth" onSubmit={onsubmit}>
       <FormField type={"text"} label={"이름"} name={name} value={name} required={true} style={{ backgroundColor: "var(--palette-gray-100)" }} />
@@ -149,35 +158,31 @@ export const SignUpFormStep = ({ onPrev, onNext }: SignUpStepProps) => {
         type={"text"}
         label={"닉네임"}
         name={"이름"}
-        value={formState.nickname.value}
+        value={formState.nick_name.value}
         placeholder="사용할 닉네임을 작성해 주세요."
         required={true}
         onChange={
           //닉네임에 대한 state변경 + 변화후 debounce를 통한 api 호출
-          handleInputChange("nickname")
+          handleInputChange("nick_name")
         }
-        onBlur={() => {
-          //닉네임 에러 검증
-          handleBlur("nickname");
-        }}
+        onBlur={handleBlur("nick_name")}
         onFocus={() => {
           /*Nickname에 대한 클라이언트 데이터 초기화 */
-          handleFocus("nickname");
+          handleFocus("nick_name");
           //닉네임 중복체크 서버 데이터 초기화
           checknickname.reset();
         }}
         errorMessage={
           //닉네임 에러 메세지 + 서버에서 온 메세지 노출-> 검증
-          formState.nickname.error || (checknickname.isError ? checknickname.error.message : "")
+          formState.nick_name.error || (checknickname.isError ? checknickname.error.message : "")
         }
         successMessage={
           //서버에서 온 메세지 노출
           checknickname.data?.message
         }
         isInvalid={
-          //검증 실패: 프론트 스키마 검증 또는 서버 중복 검증)
-          //검증 성공: 프론트 스키마 검증 성공이고 서버 중복 검증성공)
-          !!formState.nickname.isValid || checknickname.isError
+          //검증 성공: 프론트 스키마 검증 성공(formState.nick_name.isValid===false )이고 서버 중복 검증성공(checknickname.success===true)):isvalid:true
+          !(formState.nick_name.isValid && checknickname.isSuccess)
         }
       />
       <FormField
