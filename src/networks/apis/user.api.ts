efdcfdde51360d5aa4eea66589208710ee4bbf8a
euan-lee <maplestory1419@naver.com>
2025-05-29 19:48:44 +0900

    feat:네트워크 예외처리 에러메세지 수정

--- src/networks/apis/user.api.ts
+++ src/networks/apis/user.api.ts
@@ -142,12 +142,11 @@ const nicknameCheck = async (nickname: string): Promise<any> => {
   } catch (error: any) {
     if (error.response) {
       // 서버에서 응답이 왔지만 오류 상태 코드
-      console.error("닉네임 중복 확인 실패:", error.response.data);
+      throw error.response.data;
     } else {
-      // 네트워크 오류 또는 다른 문제
-      console.error("네트워크 또는 기타 오류:", error.message);
+      // 네트워크 오류 또는 다른 문제(에러메세지 커스텀 Ex 괌리자문의)
+      throw new Error("네트워크 오류가 발생했습니다. 잠시 후 다시 시도해주세요.");
     }
-    throw error.response.data; // 상위에서 try-catch 가능하게
   }
 };
 
