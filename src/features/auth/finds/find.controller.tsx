c22ee2014fb5e61587de2646350a50a385926773
LBOMI <lcil2293@naver.com>
2025-05-30 14:29:57 +0900

    재설정 시 로그인 페이지로 이동

--- src/features/auth/finds/find.controller.tsx
+++ src/features/auth/finds/find.controller.tsx
@@ -5,19 +5,19 @@ import { usePageNavigation } from "@/shared/lib/hooks";
 
 export const FindController = () => {
   // View에서 사용되어질 상태관리
-  const [step, setStep] = useState<"인증선택" | "이매일아이디보여주기" | "비밀번호재설정">("인증선택");
+  const [step, setStep] = useState<"인증선택" | "이메일아이디보여주기" | "비밀번호재설정">("인증선택");
 
   //인증선택해서 성공시 이매일아이디보여주기로 이동
   const handleOauthLogin = () => {
     // oauthMutation.mutate(undefined, {});
-    setStep("이매일아이디보여주기");
+    setStep("이메일아이디보여주기");
   };
 
   // //비밀번호 재설정 함수
   const navigation = usePageNavigation();
 
   const handleResetPassword = (payload: any) => {
-    navigation.goHome();
+    navigation.goBack();
   };
   // Controller에서 View로 내려질 Props
   const props = {
