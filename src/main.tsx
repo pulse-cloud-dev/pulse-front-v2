113be394f6b294fe68d0815867545229158eb2a6
LBOMI <lcil2293@naver.com>
2025-05-31 17:19:52 +0900

    .

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 
 import App from "./app";
 import "vite/modulepreload-polyfill";
@@ -20,8 +19,6 @@ loadNaverMapScript();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
     <App />
-    </BrowserRouter>
   </React.StrictMode>
 );
