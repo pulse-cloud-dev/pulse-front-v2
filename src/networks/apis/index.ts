d6464bcd80b37524f7c70590f114107eda516ee4
LBOMI <lcil2293@naver.com>
2025-06-30 14:49:08 +0900

    Revert "[fix]충돌 수정"
    This reverts commit d274c7a6cd2f5ac617d1c02396a9e3c7fd91987b.

--- src/networks/apis/index.ts
+++ src/networks/apis/index.ts
@@ -1,5 +1,6 @@
 export * from "./user.api";
-
+<<<<<<< HEAD
 export * from "./post.api";
-
+=======
 export * from "./category.api";
+>>>>>>> temp-merge-branch
--- src/shared/components/atoms/buttons/baseButton.tsx
+++ src/shared/components/atoms/buttons/baseButton.tsx
@@ -3,9 +3,14 @@ import { forwardRef } from "react";
 
 type BaseButtonProps = PropsWithChildren<ButtonHTMLAttributes<HTMLButtonElement>> & {
   size?: "xs" | "sm" | "md" | "lg" | "xl" | "xxl";
+<<<<<<< HEAD
+  color?: "bk" | "wh" | "default" | "primary" | "reverse";
+};
+=======
   color?: "bk" | "wh" | "default" | "primary" | "reverse" | "teal" ;
 }
 
+>>>>>>> temp-merge-branch
 export const BaseButton = forwardRef<HTMLButtonElement, BaseButtonProps>((props, forwardedRef) => {
   const { className, size = "md", color = "default", style, children, ...restProps } = props;
   return (
