3fc4d226945f6ad8c0e7c392ba72bd36add80b4b
LBOMI <lcil2293@naver.com>
2025-07-05 17:40:32 +0900

    [feat]suspense, errorboundary 추가

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,16 @@
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 import { Toaster } from "react-hot-toast";
 
 import "@/app/styles/index.css";
 import "@/shared/modules/select-ui/styles/index.css";
+
 import { QueryProvider, RouterProvider } from "@/app/contexts";
 import { ModalProvider } from "@/shared/modules";
 import { useTheme } from "@/shared/lib/hooks";
 import { keyConst } from "@/shared/constants";
+
+import { ErrorFallback } from "@/shared/boundaries"
 
 console.log(`
   '||''|.  '||'  '|' '||'       .|'''.|  '||''''|  
@@ -15,15 +20,20 @@ console.log(`
   .||.       '|..'   .||.....| |'....|'  .||.....|                                                  
   `);
 
+
 function App() {
   useTheme(keyConst.THEME);
 
   return (
     <QueryProvider>
-      <ModalProvider> 
-        <Toaster position="bottom-left" />
-        <RouterProvider />
-      </ModalProvider>
+      <ErrorBoundary FallbackComponent={ErrorFallback}>
+        <Suspense fallback={<div>로딩 중입니다...</div>}>
+          <ModalProvider> 
+            <Toaster position="bottom-left" />
+            <RouterProvider />
+          </ModalProvider>
+        </Suspense>
+      </ErrorBoundary>
     </QueryProvider>
   );
 }
--- src/app/contexts/query.provider.tsx
+++ src/app/contexts/query.provider.tsx
@@ -5,7 +5,14 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 interface Props extends PropsWithChildren {}
 
 export const QueryProvider = ({ children }: Props) => {
-  const queryClient = new QueryClient();
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 1000 * 60, 
+        retry: 1,
+      },
+    },
+  });
 
   return (
     <QueryClientProvider client={queryClient}>
