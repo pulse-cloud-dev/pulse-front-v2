7a9f880b5de33a7e84f5f9affefe5297b26baee1
LBOMI <lcil2293@naver.com>
2025-07-08 20:24:02 +0900

    refactor: 컴포넌트 분리

--- src/features/index.ts
+++ src/features/index.ts
@@ -8,7 +8,7 @@ export * from "./auth/finds/find.controller";
 
 // Mentor
 export * from "./mentor/mentor.controller";
-export * from "./mentor/mentorDetail.controller";
+export * from "./mentoringDetail/mentorDetail.controller";
 
 // Chat
 export * from "./chat/chat.controller";
