5ae3d6e27d11e9291414373373f741391e2d2e83
LBOMI <lcil2293@naver.com>
2025-07-13 19:06:25 +0900

    [fix]props 수정

--- src/features/mentoringDetail/mentorDetail.controller.tsx
+++ src/features/mentoringDetail/mentorDetail.controller.tsx
@@ -1,14 +1,16 @@
 import { useParams } from "react-router-dom";
 import { useMentoringDetailQuery } from "@/features/mentoringDetail";
 import { MentorDetailView } from "./mentorDetail.view";
+import { MentoringDetail } from "@/contracts/request/category/mentoringDetail";
 import dayjs from "dayjs";
 
-const dummyFallback = {
+const dummyFallback: MentoringDetail = {
   title: "제목 없음",
-  region: "지역 정보 없음",
+  region: "서울",
   content: "내용",
   mentor_nickname: "이름 없음",
   mentor_job: "직무 정보 없음",
+  mentor_career_total_year: 0,
   mentor_last_company: "경력 정보 없음",
   cost: 0,
   start_date: "2025-01-01",
@@ -16,9 +18,14 @@ const dummyFallback = {
   address: "주소 없음",
   detail_address: "",
   deadline_date: "2025-07-06T13:07:28.092Z",
+  deadline_time: "00:00",
   apply_number: 0,
   recruit_number: 0,
+  lecture_type: "ONLINE",
+  mentor_profile_image: "",
+  online_platform: "",
 };
+
 
 export const DetailController = () => {
   const { id } = useParams<{ id: string }>();
@@ -32,15 +39,17 @@ export const DetailController = () => {
   return (
     <MentorDetailView
       title={d.title}
-      region={d.address}
+      region={d.region}
       content={d.content}
       mentorName={d.mentor_nickname}
       mentorJob={d.mentor_job}
-      mentorCareer={d.mentor_last_company}
+      mentorCareer={d.mentor_career_total_year}
+      lastCompany={d.mentor_last_company}
       price={d.cost}
       period={`${d.start_date} ~ ${d.end_date}`}
       location={`${d.address} ${d.detail_address}`}
       deadline={`${formattedDeadline}`}
+      lectureType={d.lecture_type}
       recruitNumber={d.recruit_number}
       applyNumber={d.apply_number}
     />
