f07deef1e9442a859c4b18ad9cbdbbff32bb520e
LBOMI <lcil2293@naver.com>
2025-07-13 19:04:13 +0900

    [fix]프로필 카드

--- src/features/mentoringDetail/components/profileCard.tsx
+++ src/features/mentoringDetail/components/profileCard.tsx
@@ -1,17 +1,18 @@
 interface MentorProfileCardProps {
   name: string;
   job: string;
-  career: string;
+  career: number;
   imageUrl?: string;
+  lastCompany: string;
 }
 
-export const MentorProfileCard = ({ name, job, career, imageUrl }: MentorProfileCardProps) => (
+export const MentorProfileCard = ({ name, job, career, imageUrl, lastCompany }: MentorProfileCardProps) => (
   <div className="mentor-profile-card">
     <img src={imageUrl || "/images/profile 1.png"} alt="프로필" className="profile-img" />
     <div>
       <p className="mentor-name">{name}</p>
       <p className="mentor-info">
-        직무: {job} <br />경력: {career}
+        직무: {job} {career}년차 <br /> 경력: {lastCompany}
       </p>
     </div>
     <button className="mentor-profile-button">멘토 프로필 보기</button>
