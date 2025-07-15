03b7ef12f58dcbf5dd03e34c66dd99a4849c2973
euan-lee <maplestory1419@naver.com>
2025-07-15 20:54:24 +0900

    bug:signup step2 버튼 컴포넌트 css qa

--- src/features/auth/signUp/steps/signupcertificationstep.tsx
+++ src/features/auth/signUp/steps/signupcertificationstep.tsx
@@ -8,9 +8,9 @@ export const SignUpCertificationStep = ({ handleJoinSocial }: { handleJoinSocial
   return (
     <div className="m-t-33 w-100 flex_r align_center justify_center">
       {socialLogin.map((item) => (
-        <BaseButton className="w400 m-b-30 border gap_8" size="xl" onClick={handleJoinSocial}>
+        <BaseButton className="w400 m-b-30 border radius_10 gap_8" size="xl" onClick={handleJoinSocial} style={{ display: "flex", justifyContent: "center", alignItems: "center", padding: "16px" }}>
           <Icon src={item.icon} alt={item.alt} />
-          <Typography variant="compact" size="16" weight="regular" color="grayscale" colorscale="90" style={{ letterSpacing: "-2%", lineHeight: "100%" }}>
+          <Typography variant="compact" size="16" weight="regular" color="grayscale" colorscale="90" style={{ lineHeight: "100%" }}>
             {item.text}
           </Typography>
         </BaseButton>
