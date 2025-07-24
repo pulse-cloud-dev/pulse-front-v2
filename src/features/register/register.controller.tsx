eb17bd20f413855c71b04548abd1364b3807beb6
euan-lee <maplestory1419@naver.com>
2025-07-24 22:39:06 +0900

    feat:register 모달 추가

--- src/features/register/register.controller.tsx
+++ src/features/register/register.controller.tsx
@@ -11,15 +11,19 @@ export const RegisterContainer = () => {
   const careerState = useStack<RegisterSchema>(createInitialCareerSchema);
   const educationState = useStack<RegisterSchema>(createInitialEducationSchema);
   const certificateState = useStack<RegisterSchema>(createInitialCertificateSchema);
-  const openmodal = useModal(Alert);
-  const closemodal = useModal(Alert);
+  const openRegisterModal = useModal(RegisterAlerts.Confirm);
+  const closeRegisterModal = useModal(RegisterAlerts.Cancel);
+
+  const failRegisterModal = useModal(RegisterAlerts.Fail);
   const [introduction, setIntroduction] = useState<string>("");
   const registerMentor = useRegisterMentor({
-    onSuccess: () => openRegisterModal(),
+    onSuccess: () => openRegisterModal.openModal(),
     onError: (error) => {
       console.error("멘토 등록 실패:", error);
+      failRegisterModal.openModal();
     },
   });
+
   const allStacksValid = useMemo(() => {
     const checkStackStatus = (stacks: RegisterSchema[], skipFields?: (stack: RegisterSchema) => string[], stackName?: string): boolean => {
       let isValid = true;
@@ -70,29 +74,124 @@ export const RegisterContainer = () => {
     return careerValid && educationValid && certificateValid && jobValid;
   }, [jobState.stacks, careerState.stacks, educationState.stacks, certificateState.stacks]);
 
-  const openRegisterModal = () => {
-    openmodal.openModal({
-      title: "멘토 등록",
-      body: "멘토등록이 완료되었어요.",
-      cancelBtn: false,
-    });
-  };
-  const closeRegisterModal = () => {
-    closemodal.openModal({
-      title: "멘토 등록",
-      body: "작성중인 내용이 있습니다. 페이지를 나가실 경우 작성된 내용은 모두 삭제가 됩니다. 그래도 나가시겠습니까?",
-      onCancel: () => {
-        // ✅ 취소 시 아무것도 안 하거나 원하는 동작 실행
-        console.log("등록 취소됨");
-      },
-      onConfirm: () => {
-        // ✅ 취소 시 아무것도 안 하거나 원하는 동작 실행
-        console.log("등록 취소됨");
-      },
-    });
-  };
-
   const register = () => {
+    const mapCareerStacksToDto = (stacks: RegisterSchema[]): any[] => {
+      const mapCareerStackToRawDto = (careerStack: RegisterSchema) => ({
+        company_name: careerStack.company,
+        department: careerStack.department,
+        position: careerStack.role,
+        join_date: careerStack.startDate,
+        retire_date: careerStack.endDate,
+        is_working: careerStack.isWorking,
+      });
+      const roleLevelList = queryClient.getQueryData(["roleLevels"]) as {
+        name: string;
+        description: string;
+      }[];
+
+      const positionHash: { [key: string]: string } = Object.fromEntries(roleLevelList?.map((item) => [item.description, item.name]) || []);
+      const mapCareerRawDtoToFinalDto = (careerDto: any) => ({
+        company_name: careerDto.company_name.value,
+        department: careerDto.department.value, //해싱처리 하기
+        position: positionHash[careerDto.position.value],
+        join_date: formatDateToYYYYMM(careerDto.join_date.value),
+        retire_date: formatDateToYYYYMM(careerDto.retire_date.value),
+        is_working: careerDto.is_working.value,
+      });
+      return stacks.map(mapCareerStackToRawDto).map(mapCareerRawDtoToFinalDto);
+    };
+
+    const mapEducationStacksToDto = (stacks: RegisterSchema[]): any[] => {
+      const educationLevelList = queryClient.getQueryData(["educationLevels"]) as {
+        name: string;
+        description: string;
+      }[];
+      const educationStatusList = queryClient.getQueryData(["educationStatuses"]) as {
+        name: string;
+        description: string;
+      }[];
+      const educationLevelHash: { [key: string]: string } = Object.fromEntries(educationLevelList?.map((item) => [item.description, item.name]) || []);
+
+      const educationStatusHash: { [key: string]: string } = Object.fromEntries(educationStatusList?.map((item) => [item.description, item.name]) || []);
+      const mapEducationStackToRawDto = (educationStack: RegisterSchema) => ({
+        education_level: educationStack.schoolType,
+        school_name: educationStack.schoolName,
+        major: educationStack.major,
+        education_status: educationStack.graduationStatus,
+        admission_date: educationStack.admissionDate,
+        graduation_date: educationStack.graduationDate,
+      });
+      // raw DTO -> 최종 API DTO
+      const mapRawDtoToFinalDto = (academicDto: any) => ({
+        education_level: educationLevelHash[academicDto.education_level.value || academicDto.education_level] || "",
+        school_name: academicDto.school_name?.value || academicDto.school_name || "",
+        major: academicDto.major?.value || academicDto.major || "",
+        education_status: educationStatusHash[academicDto.education_status?.value || academicDto.education_status] || "",
+        admission_date: formatDateToYYYYMM(academicDto.admission_date?.value),
+        graduation_date: formatDateToYYYYMM(academicDto.graduation_date?.value),
+      });
+
+      return stacks.map(mapEducationStackToRawDto).map(mapRawDtoToFinalDto);
+    };
+
+    const mapJobStacksToDto = (stacks: RegisterSchema[]): any => {
+      const categoryItemMetaList = queryClient.getQueryCache().findAll({
+        predicate: (query) => query.queryKey[0] === "categoryItemMetaList",
+      });
+      const jobCodeHash: Record<string, string> = {};
+
+      categoryItemMetaList.forEach((query) => {
+        const data = query.state.data;
+        if (data && Array.isArray(data)) {
+          data.forEach((item) => {
+            if (item.name && item.code) {
+              jobCodeHash[item.name] = item.code;
+            }
+          });
+        }
+      });
+
+      const mapJobStackToRawDto = (jobStack: RegisterSchema) => ({
+        job_code: jobStack.jobDetail.value,
+      });
+
+      const mapJobRawDtoToFinalDto = (jobDto: any) => ({
+        job_code: jobCodeHash[jobDto.job_code] || "OTHER",
+      });
+      return stacks.map(mapJobStackToRawDto).map(mapJobRawDtoToFinalDto)[0];
+    };
+
+    const mapCertificationStacksToDto = (stacks: RegisterSchema[]): any[] => {
+      const mapCertificationStackToRawDto = (certificateStack: RegisterSchema) => ({
+        certificate_name: certificateStack.certificateName,
+        issuer: certificateStack.issuer,
+        pass_status: certificateStack.passStatus,
+        pass_date: certificateStack.passDate,
+      });
+
+      const passStatusList = queryClient.getQueryData(["pass-status"]) as {
+        name: string;
+        description: string;
+      }[];
+
+      const certificationHash = Object.fromEntries(passStatusList.map((item) => [item.description, item.name]));
+
+      const mapCertificationRawDtoToFinalDto = (Dto: any) => ({
+        certificate_name: Dto.certificate_name.value,
+        issuer: Dto.issuer.value,
+        pass_status: certificationHash[Dto.pass_status.value],
+        pass_date: formatDateToYYYYMM(Dto.pass_date.value),
+      });
+      return stacks.map(mapCertificationStackToRawDto).map(mapCertificationRawDtoToFinalDto);
+    };
+
+    const formatDateToYYYYMM = (date: Date | null): string | null => {
+      if (!date) return null;
+      const year = date.getFullYear();
+      const month = String(date.getMonth() + 1).padStart(2, "0");
+      return `${year}${month}`;
+    };
+
     registerMentor.mutate({
       academic_info_list: mapEducationStacksToDto(educationState.stacks),
       certificate_info_list: mapCertificationStacksToDto(certificateState.stacks),
@@ -102,121 +201,45 @@ export const RegisterContainer = () => {
     });
   };
 
-  return <RegisterView job={jobState} career={careerState} education={educationState} certificate={certificateState} introduction={{ introduction, setIntroduction }} isValid={allStacksValid} onCancel={closeRegisterModal} onSubmit={register} />;
-};
-const mapCareerStacksToDto = (stacks: RegisterSchema[]): any[] => {
-  const mapCareerStackToRawDto = (careerStack: RegisterSchema) => ({
-    company_name: careerStack.company,
-    department: careerStack.department,
-    position: careerStack.role,
-    join_date: careerStack.startDate,
-    retire_date: careerStack.endDate,
-    is_working: careerStack.isWorking,
-  });
-  const roleLevelList = queryClient.getQueryData(["roleLevels"]) as {
-    name: string;
-    description: string;
-  }[];
-
-  const positionHash: { [key: string]: string } = Object.fromEntries(roleLevelList?.map((item) => [item.description, item.name]) || []);
-  const mapCareerRawDtoToFinalDto = (careerDto: any) => ({
-    company_name: careerDto.company_name.value,
-    department: careerDto.department.value, //해싱처리 하기
-    position: positionHash[careerDto.position.value],
-    join_date: formatDateToYYYYMM(careerDto.join_date.value),
-    retire_date: formatDateToYYYYMM(careerDto.retire_date.value),
-    is_working: careerDto.is_working.value,
-  });
-  return stacks.map(mapCareerStackToRawDto).map(mapCareerRawDtoToFinalDto);
+  return (
+    <RegisterView job={jobState} career={careerState} education={educationState} certificate={certificateState} introduction={{ introduction, setIntroduction }} isValid={allStacksValid} onCancel={closeRegisterModal.openModal} onSubmit={register} />
+  );
 };
 
-const mapEducationStacksToDto = (stacks: RegisterSchema[]): any[] => {
-  const educationLevelList = queryClient.getQueryData(["educationLevels"]) as {
-    name: string;
-    description: string;
-  }[];
-  const educationStatusList = queryClient.getQueryData(["educationStatuses"]) as {
-    name: string;
-    description: string;
-  }[];
-  const educationLevelHash: { [key: string]: string } = Object.fromEntries(educationLevelList?.map((item) => [item.description, item.name]) || []);
+interface RegisterAlertProps {
+  closeModal: (id: string) => void;
+  id: string;
+}
+import { usePageNavigation } from "@/shared/lib/hooks";
 
-  const educationStatusHash: { [key: string]: string } = Object.fromEntries(educationStatusList?.map((item) => [item.description, item.name]) || []);
-  const mapEducationStackToRawDto = (educationStack: RegisterSchema) => ({
-    education_level: educationStack.schoolType,
-    school_name: educationStack.schoolName,
-    major: educationStack.major,
-    education_status: educationStack.graduationStatus,
-    admission_date: educationStack.admissionDate,
-    graduation_date: educationStack.graduationDate,
-  });
-  // raw DTO -> 최종 API DTO
-  const mapRawDtoToFinalDto = (academicDto: any) => ({
-    education_level: educationLevelHash[academicDto.education_level.value || academicDto.education_level] || "",
-    school_name: academicDto.school_name?.value || academicDto.school_name || "",
-    major: academicDto.major?.value || academicDto.major || "",
-    education_status: educationStatusHash[academicDto.education_status?.value || academicDto.education_status] || "",
-    admission_date: formatDateToYYYYMM(academicDto.admission_date?.value),
-    graduation_date: formatDateToYYYYMM(academicDto.graduation_date?.value),
-  });
-
-  return stacks.map(mapEducationStackToRawDto).map(mapRawDtoToFinalDto);
+const RegisterConfirmAlert = () => {
+  const { goToPage } = usePageNavigation();
+  return <Alert title="멘토 등록" body="멘토등록이 완료되었어요." cancelBtn={false} confirmText="확인" onConfirm={() => goToPage("/mentee-find")} />;
 };
 
-const mapJobStacksToDto = (stacks: RegisterSchema[]): any => {
-  const categoryItemMetaList = queryClient.getQueryCache().findAll({
-    predicate: (query) => query.queryKey[0] === "categoryItemMetaList",
-  });
-  const jobCodeHash: Record<string, string> = {};
+const RegisterfailAlert = () => {
+  const { goToPage } = usePageNavigation();
 
-  categoryItemMetaList.forEach((query) => {
-    const data = query.state.data;
-    if (data && Array.isArray(data)) {
-      data.forEach((item) => {
-        if (item.name && item.code) {
-          jobCodeHash[item.name] = item.code;
-        }
-      });
-    }
-  });
-
-  const mapJobStackToRawDto = (jobStack: RegisterSchema) => ({
-    job_code: jobStack.jobDetail.value,
-  });
-
-  const mapJobRawDtoToFinalDto = (jobDto: any) => ({
-    job_code: jobCodeHash[jobDto.job_code] || "OTHER",
-  });
-  return stacks.map(mapJobStackToRawDto).map(mapJobRawDtoToFinalDto)[0];
+  return <Alert title="멘토 등록" body="멘토등록은 5개 까지만 가능합니다." cancelBtn={false} confirmText="확인" onConfirm={() => goToPage("/mentee-find")} />;
 };
 
-const mapCertificationStacksToDto = (stacks: RegisterSchema[]): any[] => {
-  const mapCertificationStackToRawDto = (certificateStack: RegisterSchema) => ({
-    certificate_name: certificateStack.certificateName,
-    issuer: certificateStack.issuer,
-    pass_status: certificateStack.passStatus,
-    pass_date: certificateStack.passDate,
-  });
+const RegisterCancelAlert = ({ id, closeModal }: RegisterAlertProps) => {
+  const { goToPage } = usePageNavigation();
 
-  const passStatusList = queryClient.getQueryData(["pass-status"]) as {
-    name: string;
-    description: string;
-  }[];
-
-  const certificationHash = Object.fromEntries(passStatusList.map((item) => [item.description, item.name]));
-
-  const mapCertificationRawDtoToFinalDto = (Dto: any) => ({
-    certificate_name: Dto.certificate_name.value,
-    issuer: Dto.issuer.value,
-    pass_status: certificationHash[Dto.pass_status.value],
-    pass_date: formatDateToYYYYMM(Dto.pass_date.value),
-  });
-  return stacks.map(mapCertificationStackToRawDto).map(mapCertificationRawDtoToFinalDto);
+  return (
+    <Alert
+      title="멘토 등록"
+      body="작성중인 내용이 있습니다. 페이지를 나가실 경우 작성된 내용은 모두 삭제가 됩니다. 그래도 나가시겠습니까?"
+      cancelText="계속 작성"
+      confirmText="나가기"
+      onCancel={() => closeModal(id)}
+      onConfirm={() => goToPage("/mentee-find")}
+    />
+  );
 };
 
-const formatDateToYYYYMM = (date: Date | null): string | null => {
-  if (!date) return null;
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, "0");
-  return `${year}${month}`;
+export const RegisterAlerts = {
+  Confirm: RegisterConfirmAlert,
+  Cancel: RegisterCancelAlert,
+  Fail: RegisterfailAlert,
 };
